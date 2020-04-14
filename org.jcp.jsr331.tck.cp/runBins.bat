528d3453323cef2542fba580dcc809ca536a2bdb
Jacob Feldman <jacobfeldman@openrules.com>
2020-04-14 14:56:47 -0400

    added Sugar

--- org.jcp.jsr331.tck.cp/runBins.bat
+++ org.jcp.jsr331.tck.cp/runBins.bat
@@ -1,7 +1,8 @@
 set CLASS_NAME=org.jcp.jsr331.samples.Bins
 rem set SOLVER=Constrainer
-set SOLVER=Choco
+rem set SOLVER=Choco
 rem set SOLVER=JSetL
+set SOLVER=Sugar
 @echo off
 cd %~dp0
 call .\run
