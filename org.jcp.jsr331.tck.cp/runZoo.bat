16238d7cba2622e25e48de5646b0cdf24d833cf8
Jacob Feldman <jacobfeldman@openrules.com>
2020-04-27 06:22:50 -0400

    added sugar

--- org.jcp.jsr331.tck.cp/runZoo.bat
+++ org.jcp.jsr331.tck.cp/runZoo.bat
@@ -1,7 +1,8 @@
 set CLASS_NAME=org.jcp.jsr331.samples.Zoo
-set SOLVER=Constrainer
+rem set SOLVER=Constrainer
 rem set SOLVER=Choco
 rem set SOLVER=JSetL
+set SOLVER=sugar
 @echo off
 cd %~dp0
 call .\run
