835e2249e3857313a0b9ee9d49376a0553b0d6c4
Jacob Feldman <jacobfeldman@openrules.com>
2020-04-27 11:32:31 -0400

    added lpsolve

--- org.jcp.jsr331.tck.linear/runInsideOutsideProduction.bat
+++ org.jcp.jsr331.tck.linear/runInsideOutsideProduction.bat
@@ -1,7 +1,10 @@
 set CLASS_NAME=org.jcp.jsr331.linear.samples.InsideOutsideProduction
+
 rem set SOLVER=Scip
-set SOLVER=GLPK
-rem set SOLVER=Coin
+rem set SOLVER=GLPK
+set SOLVER=Coin
+rem set SOLVER=lpsolve
+
 @echo off
 cd %~dp0
 call .\run
--- org.jcp.jsr331.tck.linear/runKnapsack.bat
+++ org.jcp.jsr331.tck.linear/runKnapsack.bat
@@ -1,7 +1,10 @@
 set CLASS_NAME=org.jcp.jsr331.linear.samples.Knapsack
+
 set SOLVER=Scip
 rem set SOLVER=GLPK
 rem set SOLVER=Coin
+rem set SOLVER=lpsolve
+
 @echo off
 cd %~dp0
 call .\run
