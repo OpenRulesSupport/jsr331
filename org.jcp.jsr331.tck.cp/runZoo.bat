898abeee62b08caddf97ef1c769c9a771cf188e0
Jacob Feldman <jacobfeldman@openrules.com>
2020-07-05 16:37:30 -0400

    removed dependency from an external jar

--- org.jcp.jsr331.tck.cp/runZoo.bat
+++ org.jcp.jsr331.tck.cp/runZoo.bat
@@ -1,8 +1,8 @@
 set CLASS_NAME=org.jcp.jsr331.samples.Zoo
 rem set SOLVER=Constrainer
 rem set SOLVER=Choco
-rem set SOLVER=JSetL
-set SOLVER=sugar
+set SOLVER=JSetL
+rem set SOLVER=sugar
 @echo off
 cd %~dp0
 call .\run
